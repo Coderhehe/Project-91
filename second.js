a2d2d2edf2debff77aac53dc72919da3b4b4d24f
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-04-04 21:37:17 -0400

    Add files via upload

--- second.js
+++ second.js
@@ -1,3 +1,8 @@
+pl1=localStorage.getItem("player1");
+pl2=localStorage.getItem("player2");
+p1s=0;
+p2s=0;
+
 function send() {
     number1=document.getElementById("number1").value;
     number2=document.getElementById("number2").value;
@@ -9,6 +14,40 @@ function send() {
     row=question+ib+cb;
 
     document.getElementById("output").innerHTML=row;
-    document.getElementById("number1").innerHTML="";
-    document.getElementById("number2").innerHTML="";
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";
+}
+qt="player1";
+at="player2";
+function check() {
+    geta=document.getElementById("input_check_box").value;
+    if (geta==ranswer) {
+        if (at=="player1") {
+            np1s=p1s+1;
+document.getElementById("player1_score").innerHTML=pl1+":  "+np1s;
+document.getElementById("player2_score").innerHTML=pl2+":  "+np2s;
+        }
+        if (at=="player2") {
+            np2s=p2s+1;
+document.getElementById("player2_score").innerHTML=pl2+":   "+np2s;
+document.getElementById("player1_score").innerHTML=pl1+":  "+np1s;
+        }
+    }
+    if (qt=="player1") {
+        document.getElementById("pq").innerHTML="Question   :"+pl2;
+        qt="player2";
+    }
+    else  {
+        document.getElementById("pq").innerHTML="Question   :"+pl1;
+        qt="player1";
+    } 
+    if (at=="player1") {
+        document.getElementById("pa").innerHTML="Answer   :"+pl2;
+        at="player2";
+    }
+    else  {
+        document.getElementById("pa").innerHTML="Answer   :"+pl1;
+        at="player1";
+    } 
+    document.getElementById("input_check_box").value="";
 }
